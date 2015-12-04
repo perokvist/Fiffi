53a744036a7045a3239c407764f1f05981cdd5a4
Per Ökvist <per.okvist@outlook.com>
2015-12-04 11:28:47 +0100

    Init testing utils

---
+++ src/Fiffi.Testing/Env.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Fiffi.Testing
+{
+	public static class Env
+	{
+		public static async Task RunAsync(
+			Action<IApplicationBuilder> a, 
+			Action<IServiceCollection> a2,
+			UseCaseData @case)
+		{
+			await ServerFixture.UseAsync(a, a2, async (client, pub) =>
+			{
+				var happend = new List<IEvent>();
+				var assertEvents = @case.ThenEvents != null && @case.ThenEvents.Any();
+
+				if (assertEvents)
+					pub.Register(events => 
+					 {
+						 happend.AddRange(events);
+						 return Task.FromResult(0);
+					 });
+
+				await pub.PublishAsync(@case.Given.ToArray());
+
+				if (@case.When != null)
+				{
+					var r = await client.SendAsync(@case.When);
+					Assert.Equal(@case.ThenResponse, await r.Content.ReadAsStringAsync());
+				}
+
+				if (assertEvents)
+				{
+					// MessageVault.MessageReader sleeps for 1sec :I
+					if (!happend.Any())
+						await Task.Delay(1000);
+
+					Assert.Collection(@case.ThenEvents, @event => Assert.True(happend.Any(e => e.GetType() == @event.GetType())));
+				}
+			});
+		}
+
+		public static UseCaseData UseCase(string name, IEnumerable<IEvent> given, HttpRequestMessage when = null, object thenResponse = null,
+			IEnumerable<IEvent> thenEvents = null)
+		{
+			return new UseCaseData(name, given, when, thenResponse, thenEvents);
+		}
+
+		public class UseCaseData
+		{
+			public string Name;
+			public IEnumerable<IEvent> Given;
+			public HttpRequestMessage When;
+			public object ThenResponse;
+			public IEnumerable<IEvent> ThenEvents;
+
+			internal UseCaseData(string name, IEnumerable<IEvent> given, HttpRequestMessage when, object thenResponse, IEnumerable<IEvent> thenEvents)
+			{
+				Name = name;
+				Given = given;
+				When = when;
+				ThenResponse = thenResponse;
+				ThenEvents = thenEvents;
+			}
+		}
+	}
+}
---
+++ src/Fiffi.Testing/Spec.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Fiffi.Testing
+{
+	public static class Spec
+	{
+		public static IEnumerable<IEvent> GivenEvents(params IEvent[] es) => es ?? Enumerable.Empty<IEvent>();
+
+		public static HttpRequestMessage PostJSON(string url, string values)
+		{
+			return new HttpRequestMessage(HttpMethod.Post, new Uri(url))
+			{
+				Content = new StringContent(values ?? string.Empty, Encoding.UTF8, "application/json")
+			};
+		}
+
+		public static HttpRequestMessage GetJSON(string url, object values)
+		{
+			return new HttpRequestMessage(HttpMethod.Get, new Uri(url))
+			{
+				Content = new StringContent(JsonConvert.SerializeObject(values), Encoding.UTF8, "application/json")
+			};
+		}
+
+
+		public static IEnumerable<IEvent> Events(params IEvent[] es) => es ?? Enumerable.Empty<IEvent>();
+	}
+}
