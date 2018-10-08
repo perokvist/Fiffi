e2465edbee5928562266497bbd539ef3c01672c3
Per Ökvist <perokvist@users.noreply.github.com>
2018-10-08 21:49:21 +0200

    refactor eventprocessor #2

--- Fiffi/EventProcessor.cs
+++ Fiffi/EventProcessor.cs
@@ -10,9 +10,9 @@ namespace Fiffi
 {
 	public class EventProcessor
 	{
-		private readonly CommandLocks _locks;
-		private readonly Action<string> _logger;
-		private readonly List<Tuple<Type, Func<IEvent, Task>>> _h = new List<Tuple<Type, Func<IEvent, Task>>>();
+		readonly CommandLocks _locks;
+		readonly Action<string> _logger;
+		readonly List<(Type Type, Func<IEvent, Task> EventHandler)> _handlers = new List<(Type, Func<IEvent, Task>)>();
 
 		public EventProcessor() : this(new CommandLocks(), s => { })
 		{ }
@@ -29,7 +29,7 @@ namespace Fiffi
 
 		public void Register<T>(Func<T, Task> f)
 			where T : IEvent
-			=> _h.Add(new Tuple<Type, Func<IEvent, Task>>(typeof(T), @event => f((T)@event)));
+			=> _handlers.Add((typeof(T), @event => f((T)@event)));
 
 
 		public async Task PublishAsync(params IEvent[] events)
@@ -37,26 +37,26 @@ namespace Fiffi
 			if (!events.All(e => e.Meta.ContainsKey(nameof(EventMetaData.CorrelationId))))
 				throw new ArgumentException("CorrelationId required");
 
-			var h = events.SelectMany(e => _h
+			var executionContext = events.SelectMany(e => _handlers
 				.Where(DelegatefForTypeOrInterface(e))
 				.Select(ExecuteDelegate(e)))
 				.ToArray();
 
-			await Task.WhenAll(h.Select(x => x.Item1));
+			await Task.WhenAll(executionContext.Select(x => x.EventHandler));
 
 			//Only release once per aggregate
-			h.GroupBy(x => x.Item3)
+			executionContext.GroupBy(x => x.CorrelationId)
 			.Select(x => x.First())
-			.ForEach(x => _locks.ReleaseIfPresent(x.Item2, x.Item3, _logger));
+			.ForEach(x => _locks.ReleaseIfPresent(x.AggregateId, x.CorrelationId, _logger));
 		}
 
-		private static Func<Tuple<Type, Func<IEvent, Task>>, Tuple<Task, IAggregateId, Guid>> ExecuteDelegate(IEvent e)
-		 => f => new Tuple<Task, IAggregateId, Guid>(f.Item2(e), new AggregateId(e.AggregateId.ToString()), Guid.Parse(e.Meta[nameof(EventMetaData.CorrelationId)]));
+		static Func<(Type Type, Func<IEvent, Task> EventHandler), (Task EventHandler, IAggregateId AggregateId, Guid CorrelationId)> ExecuteDelegate(IEvent e)
+		 => f => (f.EventHandler(e), new AggregateId(e.AggregateId.ToString()), Guid.Parse(e.Meta[nameof(EventMetaData.CorrelationId)]));
 
-		private static Func<Tuple<Type, Func<IEvent, Task>>, bool> DelegatefForTypeOrInterface(IEvent e)
-			=> kv => kv.Item1 == e.GetType() || e.GetType().GetTypeInfo().GetInterfaces().Any(t => t == kv.Item1);
+		static Func<(Type Type, Func<IEvent, Task> EventHandler), bool> DelegatefForTypeOrInterface(IEvent e)
+			=> kv => kv.Type == e.GetType() || e.GetType().GetTypeInfo().GetInterfaces().Any(t => t == kv.Type);
 
-	
+
 	}
 
 	internal class EventMetaData
