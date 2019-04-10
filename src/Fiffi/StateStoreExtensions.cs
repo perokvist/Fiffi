490fe47ea2c5e35cf93594fb437769b168dcada7
Per Ökvist <perokvist@users.noreply.github.com>
2019-04-10 07:43:49 +0200

    renamed statestore ext

---
+++ src/Fiffi/StateStoreExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiffi
+{
+	public static class StateStoreExtensions
+	{
+		public static async Task PublishAllUnPublishedEventsAsync(this IStateStore stateManager, Func<IEvent[], Task> publish)
+			=> await 
+			(await stateManager.GetAllUnPublishedEventsAsync())
+			.Pipe(async x => await stateManager.OnPublish(publish)(x)); 
+		public static Func<IEvent[], Task> OnPublish(this IStateStore stateManager, Func<IEvent[], Task> publish)
+			=> events =>
+				Task.WhenAll(events
+				.GroupBy(x => x.GetKey())
+				.Select(async x =>
+				{
+					await publish(x.ToArray());
+					var state = stateManager.GetOutBoxAsync(x.Key.SourceId);
+					await stateManager.ClearOutBoxAsync(x.Key.SourceId, x.Key.CorrelationId);
+				}));
+
+        static (string SourceId, Guid CorrelationId) GetKey(this IEvent @event) => (@event.SourceId, @event.GetCorrelation());
+	}
+}
