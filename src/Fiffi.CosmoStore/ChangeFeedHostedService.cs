7ec06189e85b90b1ccf6971a8c5d9c5d0c67feae
Per Ökvist <perokvist@users.noreply.github.com>
2019-08-12 13:26:07 +0200

    changefeed hostingservice

---
+++ src/Fiffi.CosmoStore/ChangeFeedHostedService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.Documents.ChangeFeedProcessor.PartitionManagement;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fiffi.CosmoStore
+{
+    public class ChangeFeedHostedService : IHostedService
+    {
+        private readonly Func<Task<IChangeFeedProcessor>> processorProvider;
+        private readonly ILogger<ChangeFeedHostedService> logger;
+        private IChangeFeedProcessor processor;
+
+        public ChangeFeedHostedService(Func<Task<IChangeFeedProcessor>> processorProvider, ILogger<ChangeFeedHostedService> logger)
+        {
+            this.processorProvider = processorProvider;
+            this.logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"Starting {nameof(ChangeFeedHostedService)}");
+            this.processor = await this.processorProvider();
+            await this.processor.StartAsync();
+            logger.LogInformation($"Started {nameof(ChangeFeedHostedService)}");
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"Stopping {nameof(ChangeFeedHostedService)}");
+            await this.processor.StopAsync();
+            logger.LogInformation($"Stopped {nameof(ChangeFeedHostedService)}");
+        }
+    }
+}
---
+++ src/Fiffi.CosmoStore/Configuration/Extensions.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Azure.Documents.ChangeFeedProcessor.PartitionManagement;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiffi.CosmoStore.Configuration
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddModule<TModule, TOptions>(this
+            IServiceCollection sc,
+            IConfiguration configuration,
+            Func<IServiceProvider, IEventStore, TModule> moduleFactory,
+            Action<TOptions> options)
+            where TModule : class
+            where TOptions : ModuleOptions, new()
+            => sc
+                .Tap(x =>
+                x.AddOptions<TOptions>()
+                .Bind(configuration)
+                .Configure(options)
+                .ValidateDataAnnotations())
+            .AddSingleton<IEventStore>(sp => sp.GetRequiredService<IOptions<TOptions>>()
+            .Value.Pipe(c => new CosmoStoreEventStore(c.ServiceUri, c.Key, c.TypeResolver)))
+            .AddSingleton(sp => moduleFactory(sp, sp.GetRequiredService<IEventStore>()));
+
+
+        public static IServiceCollection AddChangeFeedSubsciption<TModule, TOptions>(
+            this IServiceCollection sc,
+            Func<IServiceProvider, TModule, ILogger, Func<IEvent[], Task>> dispatcherProvider)
+            where TModule : class
+            where TOptions : ModuleOptions, new()
+            => sc
+            .AddSingleton<Func<Task<IChangeFeedProcessor>>>(sp => async () =>
+            {
+                var opt = sp.GetRequiredService<IOptions<TOptions>>().Value;
+                var logger = sp.GetRequiredService<ILogger<TModule>>();
+                var module = sp.GetRequiredService<TModule>();
+                var cf = await ChangeFeed
+                .CreateProcessorAsync(opt.ServiceUri, opt.Key, opt.HostName, opt.TypeResolver, dispatcherProvider(sp, module, logger));
+                return cf;
+            })
+            .AddHostedService<ChangeFeedHostedService>();
+
+
+
+        public class ModuleOptions
+        {
+            public ModuleOptions()
+            {
+                TypeResolver = Fiffi.TypeResolver.Default();
+            }
+
+            [Required]
+            public Uri ServiceUri { get; set; }
+
+            [Required]
+            public string Key { get; set; }
+
+            [Required]
+            public string HostName { get; set; }
+
+            public Func<string, Type> TypeResolver { get; set; }
+        }
+
+    }
+}
