e003e82b16b1ef17366cbccb95db1ade773dc2eb
Per Ökvist <perokvist@users.noreply.github.com>
2020-05-04 10:53:21 +0200

    File base TDD events

---
+++ samples/TTD/TTD.Domain/Events.cs
@@ -0,0 +1,50 @@
+﻿using Fiffi;
+using System;
+using System.Collections.Generic;
+
+namespace TTD.Domain
+{
+
+    public class Event : IEvent
+    {
+        public EventType EventName { get; set; }
+        public int Time { get; set; }
+        public int TransportId { get; set; }
+        public Kind Kind { get; set; }
+        public Location Location { get; set; }
+        public Location Destination { get; set; }
+        public Cargo[] Cargo { get; set; }
+
+        public string SourceId { get; set; } = Guid.NewGuid().ToString();
+
+        public IDictionary<string, string> Meta { get; set; } = new Dictionary<string, string>();
+    }
+
+    public class Cargo
+    {
+        public int CargoId { get; set; }
+        public Location Destination { get; set; }
+        public Location Origin { get; set; }
+    }
+
+    public enum EventType
+    { 
+        ARRVIVE = 10,
+        DEPART = 20
+    }
+
+    public enum Kind
+    { 
+        Truck = 10,
+        Ship = 20
+    }
+
+    public enum Location
+    { 
+        Factory = 0,
+        Port = 10,
+        A = 20,
+        B = 30
+    }
+
+}
---
+++ samples/TTD/TTD.Tests/UnitTest1.cs
@@ -0,0 +1,125 @@
+using Fiffi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TTD.Domain;
+using Xunit;
+
+namespace TTD.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async System.Threading.Tasks.Task Test1Async()
+        {
+            var events = new List<Event>
+            {
+                new Event {
+                    EventName = EventType.DEPART,
+                    Time = 0,
+                    TransportId = 0,
+                    Kind = Kind.Truck,
+                    Location = Location.Factory,
+                    Destination = Location.Port,
+                    Cargo = new [] { new Cargo { CargoId = 0, Destination = Location.A, Origin = Location.Factory } }
+                },
+               new Event {
+                    EventName = EventType.DEPART,
+                    Time = 0,
+                    TransportId = 1,
+                    Kind = Kind.Truck,
+                    Location = Location.Factory,
+                    Destination = Location.B,
+                    Cargo = new [] { new Cargo { CargoId = 1, Destination = Location.B, Origin = Location.Factory } }
+                },
+               new Event {
+                    EventName = EventType.ARRVIVE,
+                    Time = 1,
+                    TransportId = 0,
+                    Kind = Kind.Truck,
+                    Location = Location.Port,
+                    Cargo = new [] { new Cargo { CargoId = 0, Destination = Location.A, Origin = Location.Factory } }
+                },
+               new Event {
+                    EventName = EventType.DEPART,
+                    Time = 1,
+                    TransportId = 0,
+                    Kind = Kind.Truck,
+                    Location = Location.Port,
+                    Destination = Location.Factory
+                },
+               new Event {
+                    EventName = EventType.DEPART,
+                    Time = 1,
+                    TransportId = 2,
+                    Kind = Kind.Ship,
+                    Location = Location.Port,
+                    Destination = Location.A,
+                    Cargo = new [] { new Cargo { CargoId = 0, Destination = Location.A, Origin = Location.Factory } }
+                },
+               new Event {
+                    EventName = EventType.ARRVIVE,
+                    Time = 2,
+                    TransportId = 0,
+                    Kind = Kind.Truck,
+                    Location = Location.Factory
+                },
+               new Event {
+                    EventName = EventType.ARRVIVE,
+                    Time = 5,
+                    TransportId = 1,
+                    Kind = Kind.Truck,
+                    Location = Location.B,
+                    Cargo = new [] { new Cargo { CargoId = 1, Destination = Location.B, Origin = Location.Factory } }
+                },
+                new Event {
+                    EventName = EventType.DEPART,
+                    Time = 5,
+                    TransportId = 1,
+                    Kind = Kind.Truck,
+                    Location = Location.B,
+                    Destination = Location.Factory
+                },
+                new Event {
+                    EventName = EventType.DEPART,
+                    Time = 5,
+                    TransportId = 1,
+                    Kind = Kind.Truck,
+                    Location = Location.B,
+                    Destination = Location.Factory
+                },
+                new Event {
+                    EventName = EventType.ARRVIVE,
+                    Time = 5,
+                    TransportId = 2,
+                    Kind = Kind.Ship,
+                    Location = Location.A,
+                    Cargo = new [] { new Cargo { CargoId = 0, Destination = Location.A, Origin = Location.Factory } }
+                },
+                new Event {
+                    EventName = EventType.DEPART,
+                    Time = 5,
+                    TransportId = 2,
+                    Kind = Kind.Ship,
+                    Location = Location.A,
+                    Destination = Location.Port
+                },
+            };
+
+            var eventsWithMeta = events.Select(e => e.Tap(x => e.Meta.AddTypeInfo(e))).ToArray();
+            var store = new Fiffi.FileSystem.FileSystemEventStore("teststore", TypeResolver.FromMap(TypeResolver.GetEventsFromTypes(typeof(Event))));
+            _ = await store.AppendToStreamAsync("all", eventsWithMeta);
+
+            //{ "event": "DEPART", "time": 0, "transport_id": 0, "kind": "TRUCK", "location": "FACTORY", "destination": "PORT", "cargo": [{"cargo_id": 0, "destination": "A", "origin": "FACTORY"}]}
+            //{"event": "DEPART", "time": 0, "transport_id": 1, "kind": "TRUCK", "location": "FACTORY", "destination": "B", "cargo": [{"cargo_id": 1, "destination": "B", "origin": "FACTORY"}]}
+            //{"event": "ARRIVE", "time": 1, "transport_id": 0, "kind": "TRUCK", "location": "PORT", "cargo": [{"cargo_id": 0, "destination": "A", "origin": "FACTORY"}]}
+            //{"event": "DEPART", "time": 1, "transport_id": 0, "kind": "TRUCK", "location": "PORT", "destination": "FACTORY"}
+            //{"event": "DEPART", "time": 1, "transport_id": 2, "kind": "SHIP", "location": "PORT", "destination": "A", "cargo": [{"cargo_id": 0, "destination": "A", "origin": "FACTORY"}]}
+            //{"event": "ARRIVE", "time": 2, "transport_id": 0, "kind": "TRUCK", "location": "FACTORY"}
+            //{"event": "ARRIVE", "time": 5, "transport_id": 1, "kind": "TRUCK", "location": "B", "cargo": [{"cargo_id": 1, "destination": "B", "origin": "FACTORY"}]}
+            //{"event": "DEPART", "time": 5, "transport_id": 1, "kind": "TRUCK", "location": "B", "destination": "FACTORY"}
+            //{"event": "ARRIVE", "time": 5, "transport_id": 2, "kind": "SHIP", "location": "A", "cargo": [{"cargo_id": 0, "destination": "A", "origin": "FACTORY"}]}
+            //{"event": "DEPART", "time": 5, "transport_id": 2, "kind": "SHIP", "location": "A", "destination": "PORT"}
+        }
+    }
+}
