edf84708e829223f0fd406b1a97f364206439374
Per Ökvist <perokvist@users.noreply.github.com>
2022-12-16 09:19:15 +0100

    config extensions

---
+++ src/Fiffi/Modularization/ConfigExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Fiffi.Modularization;
+public static class ConfigExtensions
+{
+    public static Configuration<T> Triggers<T>(
+        this Configuration<T> config,
+        Func<IEvent, ICommand?> f)
+        where T : Module
+        => config.Triggers(e => Task.FromResult(f(e)));
+
+    public static Configuration<T> Triggers<T>(
+      this Configuration<T> config,
+      Func<IEvent, Task<ICommand?>> f)
+      where T : Module
+      => config.Tap(x => x.Triggers(async (events, d) =>
+      {
+          foreach (var e in events)
+          {
+              ICommand cmd = await f(e);
+              if (cmd != null)
+                  await d(e, cmd);
+          }
+      }));
+
+}
